25f2ecdd04482cf076781c3e61dbf40016fa1cbe
Bob Holt <bobholt@gmail.com>
2013-05-15 10:24:41 -0400

    pass shape tests

--- src/tiny-jquery.js
+++ src/tiny-jquery.js
@@ -1,2 +1,21 @@
 
 // Your implementation here.
+(function( window, undefined) {
+
+    'use strict';
+
+    window.$$ = window.tQuery = function( selector ) {
+
+        var tQuery = function() {
+
+        };
+
+        tQuery.forEach = function() {
+
+        };
+
+        return tQuery;
+
+    };
+
+}( window ));
